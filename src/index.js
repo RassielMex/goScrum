5e3ee384729794556325cffb5a4b97b845922212
RassielMex <rhcp94.rm@gmail.com>
2022-08-12 23:39:25 -0500

    changed: hashrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./store/store";
@@ -10,10 +10,10 @@ import { store } from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="goScrum/">
+    <HashRouter basename="goScrum/">
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
