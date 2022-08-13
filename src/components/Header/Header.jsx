4c5712dccf7efec4cdf67e642edb1ed8a233e487
RassielMex <rhcp94.rm@gmail.com>
2022-08-13 11:58:57 -0500

    text styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Container, Navbar } from "react-bootstrap";
 import { BiLogOut } from "react-icons/bi";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import "./Header.css";
+import "./Header.scss";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -24,9 +24,13 @@ const Header = () => {
           <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav> */}
-        <Container className="d-flex justify-content-end">
-          <p className="me-1 text-light">Tareas creadas:{tasks?.length}</p>
-          <p className="me-1 text-light">{localStorage.getItem("userName")}</p>
+        <Container className="d-flex justify-content-end align-content-center align-items-center">
+          <p className="me-1 mb-0 text-light ">
+            Tareas creadas:{tasks?.length}
+          </p>
+          <p className="me-1 mb-0 text-light">
+            {localStorage.getItem("userName")}
+          </p>
           <BiLogOut size={"1.5em"} color="white" onClick={handleLogOut} />
         </Container>
       </Container>
