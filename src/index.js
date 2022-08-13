8092e84ff64dfb8d0083521709d1c6b1ae6a6533
RassielMex <rhcp94.rm@gmail.com>
2022-08-12 23:41:17 -0500

    basenamer

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="goScrum/">
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
